843678a9507ac14f99efb43ef08372f022c76fb2
Diego Gaspar <diegogaspardacruz1@gmail.com>
2025-03-02 11:46:00 -0300

    chore: add config eslint, prettier and husky

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import prettier from "eslint-config-prettier";
+import pluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: ["node_modules", "dist", ".next"],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "prettier": pluginPrettier,
+    },
+    rules: {
+      ...prettier.rules,
+      "prettier/prettier": ["error", {
+        semi: false,
+        trailingComma: "all",
+        tabWidth: 2,
+        printWidth: 100,
+      }],
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+      "import/no-anonymous-default-export": "off"
+    },
+  }
+];
+
+export default eslintConfig;
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fit Flow",
+  description: "Ação é a chave fundamental para todo o sucesso.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
