c5f624101ce369076c6307277437ea7796fe768f
JoaoTaranto <tarantoj04@gmail.com>
2025-03-07 23:17:17 -0300

    feat: add 'React Hook Form' management

--- src/app/new-flow/page.tsx
+++ src/app/new-flow/page.tsx
@@ -1,47 +1,48 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+
 import Header from "../_components/header";
 
 const NewFlow = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <div className="h-screen bg-[#f4f4f4]">
       <Header />
-      <section className="flex flex-col items-center justify-center gap-5 px-[30px] pb-5">
+      <div className="flex flex-col items-center justify-center gap-5 px-[30px] pb-5">
         <h1 className="pt-[30px] font-bold">Novo Flow</h1>
-        <div className="flex max-h-[230px] max-w-[330px] flex-col gap-3.5">
-          <div>
-            <h3>Título</h3>
-            <input
-              className="w-[330px] rounded-[6px] border-[#E4E4E4] bg-white p-2.5 outline-1"
-              type="text"
-              placeholder="Digite um título:"
-              required
-            />
-          </div>
-          <div>
-            <label>Nível</label>
-            <select className="w-[330px] rounded-[6px] bg-white p-3 outline-1">
-              <option disabled>Selecione um nível:</option>
-              <option>Iniciante</option>
-              <option>Intermediário</option>
-              <option>Avançado</option>
-            </select>
-          </div>
-          <div>
-            <h3>Capa</h3>
-            <input
-              className="w-[330px] rounded-[6px] bg-white p-2.5 outline-1"
-              type="file"
-              id="coverFile"
-              required
-            />
-          </div>
-        </div>
-        <button
-          className="mt-2.5 h-[40px] w-[330px] rounded-md bg-black px-4 py-2 text-white"
-          type="submit"
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+          className="flex max-h-[230px] max-w-[330px] flex-col gap-3.5"
         >
-          Criar
-        </button>
-      </section>
+          <input
+            className="w-[330px] rounded-[6px] border-[#E4E4E4] bg-white p-2.5 placeholder-black outline-1"
+            {...register("title")}
+            type="text"
+            placeholder="Título"
+          />
+          <select
+            {...register("level")}
+            className="w-[330px] rounded-[6px] bg-white p-3 outline-1"
+          >
+            <option disabled>Selecione um nível:</option>
+            <option value="beginner">Iniciante</option>
+            <option value="intermediary">Intermediário</option>
+            <option value="advanced">Avançado</option>
+          </select>
+
+          <input
+            placeholder="Capa"
+            accept="image/*"
+            type="file"
+            className="w-[330px] rounded-[6px] bg-white p-2.5 outline-1"
+          />
+          <input type="submit" />
+        </form>
+      </div>
     </div>
   );
 };
