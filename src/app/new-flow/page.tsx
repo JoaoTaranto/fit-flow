3091a79bba840debf88475c8ec68b69c9e52b81e
JoaoTaranto <tarantoj04@gmail.com>
2025-03-14 22:19:36 -0300

    fix: fix missing image input

--- src/app/new-flow/page.tsx
+++ src/app/new-flow/page.tsx
@@ -16,7 +16,7 @@ const NewFlow = () => {
   });
 
   const onSubmit = (data: newFlowData) => {
-    console.log("Form submitted:", data);
+    alert(`Form submitted: ${data}`);
   };
 
   return (
@@ -24,10 +24,7 @@ const NewFlow = () => {
       <Header />
       <div className="flex flex-col items-center justify-center gap-5 px-[30px] pb-5">
         <h1 className="pt-[30px] font-bold">Novo Flow</h1>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex max-h-[230px] max-w-[330px] flex-col gap-3.5"
-        >
+        <form className="flex max-h-[230px] max-w-[330px] flex-col gap-3.5">
           <input
             className="w-[330px] rounded-[6px] border-[#E4E4E4] bg-white p-2.5 placeholder-black outline-1"
             {...register("title")}
@@ -54,17 +51,25 @@ const NewFlow = () => {
             <p className="text-red-500">{errors.level.message}</p>
           )}
 
+          <label
+            className="w-[330px] rounded-[6px] bg-white p-2.5 outline-1"
+            htmlFor="imageInput"
+          >
+            Selecione uma imagem:
+          </label>
           <input
             placeholder="Capa"
             accept="image/*"
             type="file"
-            className="w-[330px] rounded-[6px] bg-white p-2.5 outline-1"
+            id="imageInput"
+            className="hidden"
           />
           {errors.coverImage && (
             <p className="text-red-500">{errors.coverImage.message}</p>
           )}
 
           <button
+            onSubmit={handleSubmit(onSubmit)}
             type="submit"
             className="w-[330px] rounded-[6px] bg-black p-2.5 text-white outline-1"
           >
