452347db5c47a29f659d4272438446cef9a6bec4
JoaoTaranto <tarantoj04@gmail.com>
2025-03-17 10:18:39 -0300

    fix: use the same size icons of HomePage icons

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,13 +2,13 @@ import { ArrowLeftIcon, UserIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="flex h-[100px] items-center justify-around bg-white pr-[1.875rem] pl-[1.875rem]">
+    <header className="flex h-[80px] items-center justify-around bg-white pr-[1.875rem] pl-[1.875rem]">
       <button>
-        <ArrowLeftIcon className="font-bold" />
+        <ArrowLeftIcon size={32} className="font-bold" />
       </button>
       <h1 className="font-bold">Fit Flow</h1>
       <button>
-        <UserIcon className="font-bold" fill="black" />
+        <UserIcon size={32} className="font-bold" fill="black" />
       </button>
     </header>
   );
